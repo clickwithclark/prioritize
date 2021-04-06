f42ffbdeebd059131ff76c64ab9987f658dbf8c3
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2021-04-06 17:38:18 -0400

    fix: allow service worker to properly serve fonts

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('./serviceworker.js')
-      .then((registration) => console.log(`Service Worker Registered!😁 \n ${registration.scope}`))
+      .then((registration) => console.log(`Service Worker Registered!...😁 \n ${registration.scope}`))
       .catch((error) => console.error(`service worker error:😭${error}`));
   });
 }
@@ -34,78 +34,51 @@ function preCacheServiceWorker() {
   /* It will not cache and also not reject for individual resources that failed to be added in the cache. unlike fillServiceWorkerCache which stops caching as soon as one problem occurs. see http://stackoverflow.com/questions/41388616/what-can-cause-a-promise-rejected-with-invalidstateerror-here */
   return caches
     .open(cacheName)
-    .then((cache) => Promise.all(cacheAssets.map((url) => cache.add(url).catch((reason) => console.error([`${url} failed: ${String(reason)}`])))));
+    .then((cache) =>
+      Promise.all(cacheAssets.map((url) => cache.add(url).catch((reason) => console.table([`Service Worker: ${url} failed...😭 ${String(reason)}`]))))
+    );
 }
 
 // Call Install Event
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
+  console.log('Service Worker: Installed...⚡');
 
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('Service Worker: pre-caching assets');
-      preCacheServiceWorker();
-    })
-    // .then(() => self.skipWaiting())
-  );
-});
-
-// Call Activate Event
-self.addEventListener('fetch', (event) => {
-  // console.log('Service Worker: Fetching...🦅');
-  event.respondWith(
     caches
-      .match(event.request)
-      .then(
-        (response) =>
-          response ??
-          fetch(event.request).then((res) => {
-            // make clone of response
-            const clonedResponse = res.clone();
-            // open cache
-            caches.open(cacheName).then((cache) => {
-              // add response to cache
-              cache.put(event.request, clonedResponse);
-            });
-            return response;
-          })
-      )
-      .catch(
-        (
-          err // load default page as generic fallback
-        ) => {
-          const path = new URL(event.request.url).pathname;
-          console.log('currPath', { path });
-          caches.match(path);
-        }
-      )
+      .open(cacheName)
+      .then((cache) => {
+        console.log('Service Worker: pre-caching assets...💾');
+        preCacheServiceWorker();
+      })
+      // Call Activate Event
+      .then(() => self.skipWaiting())
   );
 });
 
-// // cached response
-// self.addEventListener('fetch', (event) => {
-//   console.log('Service Worker: Fetching...🦅');
-//   event.respondWith(
-//     caches
-//       .match(event.request)
-//       .then(
-//         (response) =>
-//           response ??
-//           fetch(event.request).then((res) => {
-//             // make clone of response
-//             const clonedResponse = res.clone();
-//             // open cache
-//             caches.open(cacheName).then((cache) => {
-//               // add response to cache
-//               cache.put(event.request, clonedResponse);
-//             });
-//             return response;
-//           })
-//       )
-//       .catch(
-//         (err) =>
-//           // load default page as generic fallback
-//           caches.match('/') ?? caches.match('/index.html')
-//       )
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  console.table('Service Worker: Fetching...🦅');
+  event.respondWith(
+    (async () => {
+      try {
+        const cacheResponse = await caches.match(event.request);
+
+        // get response from cache if it exists
+        if (cacheResponse) {
+          return cacheResponse;
+        }
+        // if it does not exists try to get response from network
+        const netWorkResponse = await fetch(event.request);
+        // make clone of response
+        const clonedResponse = netWorkResponse.clone();
+        const cache = await caches.open(cacheName);
+        // add response to cache incase missed by precache
+        cache.put(event.request, clonedResponse);
+        return netWorkResponse;
+      } catch (error) {
+        // load cached page in event of network failure
+        const path = new URL(event.request.url).pathname;
+        return caches.match(path).then((cachedResponse) => cachedResponse);
+      }
+    })()
+  );
+});

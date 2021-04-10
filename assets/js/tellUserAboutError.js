96c187c88adf7d2cab7e6a4732fda44865088c8b
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2021-04-10 11:00:40 -0400

    refactor(tellUserAboutError.js): add ability to customize time delay

--- assets/js/tellUserAboutError.js
+++ assets/js/tellUserAboutError.js
@@ -1,16 +1,22 @@
-export function tellUserAboutError(Error) {
-  const speechBubble = document.querySelector('#info-bubble');
-  const speechMessage = document.querySelector('#info-message');
+/**
+ * Displays error message to user
+ * @function tellUserAboutError
+ * @param  {Error} error - the error caught
+ * @param  {type} seconds - how long to display error to user in seconds
+ */
+export function tellUserAboutError(error, seconds = 3) {
+  const speechBubble = document.querySelector('.bubble');
+  const speechMessage = document.querySelector('.info-message');
   const logo = document.querySelector('#logo');
   window.scrollTo({ top: 0, behavior: 'smooth' });
   logo.src = '../assets/img/oops.png';
   speechBubble.style.visibility = 'visible';
-  speechMessage.textContent = Error.message;
+  speechMessage.textContent = error.message;
   setTimeout(() => {
     speechBubble.style.visibility = 'hidden';
     speechMessage.textContent = '';
     // needed to remove shine from hair that stays when image is just replaced!
     logo.src = '';
     logo.src = '../assets/img/cwc.png';
-  }, 3000);
+  }, seconds * 1000);
 }
